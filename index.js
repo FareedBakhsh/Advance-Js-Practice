8df39f862beb47737959e984506a24dd40142d2f
Fareed Bakhsh <fareedbakhsh373@gmail.com>
2023-12-16 16:48:34 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+console.log("Well come Towards the Alfah Tech Agency ");
+let a = 90;
+let b = 7;
+let c = a * b;
+console.log(c);
+let d = 5;
+let e = 3;
+console.log(d *= 2);
+// Assignment 
+//Exercise 5:  Write a program that determines if a person is eligible to vote based on their age.
+let age = 30;
+if (age >= 18) {
+    console.log('you are eligible for vote.');
+}
+else {
+    console.log('you are not eligible for vote.');
+}
+;
+// Exercise 4:   Write a program that checks if a given number is even or odd.
+let num = 10;
+if (num % 2 == 0) {
+    console.log("Given num is even");
+}
+else {
+    console.log("Given num odd");
+}
+;
+//Exercise 6:  Write a program that calculates the result of a mathematical expression.	((10 + 5) * 3 - 2) / (4 % 3)  - 7)
+let result = ((10 + 5) * 3 - 2) / (4 % 3) - 7;
+// The mathimatical expression will be:
+console.log("The Mathimatical Expression :", result);
+//Exercise 3:  Write a program that checks if a given number is positive, negative, or zero.
+let num2 = 2;
+if (num2 > 0) {
+    console.log("The num2 is postive");
+}
+else if (num2 < 0) {
+    console.log("The num2 is negative");
+}
+else {
+    console.log("The  num2 is zero");
+}
+;
+///Exercise 1:  Write a program that calculates the area of a rectangle.
+let width = 9;
+let lengthrectangle = 15;
+let reasultrectangleArea = lengthrectangle * width;
+console.log(reasultrectangleArea);
+///Exercise 2: Write a program that takes input and calculates the volume of a cube.
+// By taking the a cube for finding  its volume        
+let l = 6;
+let h = 5;
+let w = 4;
+let resaultVolumeCube = l * h * w;
+console.log(resaultVolumeCube);
